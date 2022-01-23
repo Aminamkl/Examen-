79eaf0f152927741a40551af31cd991d6980fc90
Amina Maakoul <maakoulamina@gmail.com>
2022-01-23 22:41:01 +0100

    commit 1

--- EXAMEN-FE/src/App.svelte
+++ EXAMEN-FE/src/App.svelte
@@ -27,7 +27,6 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	</head>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<body>
 	    <table class="table">
 			<thead>
